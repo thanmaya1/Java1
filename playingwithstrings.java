d804dbf033651a0d96a98465b9aeedea91d9e3cd
Thanmaya Garimella <112673461+thanmaya1@users.noreply.github.com>
2022-12-29 10:46:58 +0530

    Create playingwithstrings.java

---
+++ playingwithstrings.java
@@ -0,0 +1,17 @@
+package gh;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("than \nthana");
+String phrase="Giraffe Academy";
+System.out.println(phrase.toUpperCase());
+System.out.println(phrase.toLowerCase());
+System.out.println(phrase.length());
+System.out.println(phrase.contains("Academy"));
+System.out.println(phrase.contains("dog"));
+System.out.println(phrase.charAt(1));
+}
+
+}
